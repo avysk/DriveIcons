f6315cee2597721c5f06cbb300bc7618b1ce4da4
Alexey Vyskubov <alexey@ocaml.nl>
2022-11-07 10:56:43 +0200

    Added skeleton for CLI F# project

---
+++ RegistryCLI/Program.fs
@@ -0,0 +1,7 @@
+﻿open RegistryLib
+
+let localMachine = new WindowsRegistryKey(
+    Microsoft.Win32.Registry.LocalMachine
+)
+let driveIcons = new DriveIcons( localMachine)
+driveIcons['A'] |> printfn "%s"
