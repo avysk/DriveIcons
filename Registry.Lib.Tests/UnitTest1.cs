42b93196a226ae86f9e52ca840ba5ed75fdb11b2
Alexey Vyskubov <alexey@ocaml.nl>
2022-11-09 14:32:58 +0200

    Use Ninject for dependency injection.

--- Registry.Lib.Tests/UnitTest1.cs
+++ Registry.Lib.Tests/UnitTest1.cs
@@ -1,7 +1,20 @@
-namespace RegistryLibTest;
+using Fi.Pentode.MockedRegistry;
+
+using Ninject;
+
+namespace Fi.Pentode.Registry.Lib.Test;
 
 public class UnitTest1
 {
+    private readonly IRegistryKey _registry;
+
+    public UnitTest1()
+    {
+        IKernel kernel = new StandardKernel();
+        kernel.Bind<IRegistryKey>().To<MockedRegistryKey>();
+        _registry = kernel.Get<IRegistryKey>();
+    }
+
     [Fact]
     public void Test1()
     {
