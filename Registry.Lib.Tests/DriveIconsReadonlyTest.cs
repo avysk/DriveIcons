7daf89f20ddde4157861d82f0fb76285def1269e
Alexey Vyskubov <alexey@ocaml.nl>
2022-11-14 11:41:31 +0200

    Proper throwing tests on wrong disk char.

--- Registry.Lib.Tests/DriveIconsReadonlyTest.cs
+++ Registry.Lib.Tests/DriveIconsReadonlyTest.cs
@@ -1,18 +1,78 @@
 using Fi.Pentode.MockedRegistry;
 
 using Ninject;
+using System.Collections;
 
 namespace Fi.Pentode.Registry.Lib.Test;
 
 public class DriveIconsReadonlyTest
 {
     private const string _localMachine = "LocalMachine";
-    private readonly DriveIcons _driveIcons;
+    public readonly DriveIcons _driveIcons;
 
-    private class TestDataGenerator :
+    public class WrongDiskTestData : IEnumerable<object[]>
+    {
+        private readonly DriveIcons _driveIcons;
 
-    private const char _smallDisk = (char)('A' - 1);
-    private const char _largeDisk = (char)('Z' + 1);
+        public WrongDiskTestData()
+        {
+            IKernel kernel = new StandardKernel();
+            kernel
+                .Bind<IRegistryKey>()
+                .To<MockedRegistryKey>()
+                .WithConstructorArgument("path", "localMachine")
+                .WithConstructorArgument(
+                    "keys",
+                    new Dictionary<string, string[]>
+                    {
+                        { _localMachine, Array.Empty<string>() }
+                    }
+                )
+                .WithConstructorArgument(
+                    "values",
+                    new Dictionary<string, object>()
+                );
+            _driveIcons = kernel.Get<DriveIcons>();
+        }
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            foreach (
+                var disk in new char[] { (char)('A' - 1), (char)('Z' + 1) }
+            )
+            {
+                yield return new object[]
+                {
+                    disk,
+                    () =>
+                    {
+                        _ = _driveIcons[disk];
+                        return;
+                    }
+                };
+                yield return new object[]
+                {
+                    disk,
+                    () =>
+                    {
+                        _driveIcons[disk] = "foo";
+                        return;
+                    }
+                };
+                yield return new object[]
+                {
+                    disk,
+                    () =>
+                    {
+                        _driveIcons[disk] = null;
+                        return;
+                    }
+                };
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
 
     public DriveIconsReadonlyTest()
     {
@@ -36,85 +96,14 @@ public class DriveIconsReadonlyTest
     }
 
     [Theory]
-    [InlineData(
-        _smallDisk,
-        (Action<char>)((disk) =>
-        {
-            {
-                _ = _driveIcons[disk];
-                return;
-            }
-        })
-    )]
-    [InlineData(
-        _largeDisk,
-        (disk) =>
-        {
-            (char disk) => _ = _driveIcons[disk];
-        }
-    )]
+    [ClassData(typeof(WrongDiskTestData))]
     public void DriveIcons_DiskCharIsNotInRange_AccessorsThrow(
         char disk,
-        Func<char, Action> useDisk
+        Action action
     )
     {
-        var code = useDisk(disk);
-        var exception = Assert.Throws<RegistryException>(code);
+        var exception = Assert.Throws<RegistryException>(action);
         Assert.IsType<RegistryException>(exception);
         Assert.Equal($"Disk {disk} is not in range.", exception.Message);
-    }
-
-    [Fact]
-    public void DriveIcons_DiskCharIsTooSmall_WritingPathThrows()
-    {
-        var exception = Assert.Throws<RegistryException>(
-            () => _driveIcons[_smallDisk] = "foo"
-        );
-        ;
-        Assert.IsType<RegistryException>(exception);
-        Assert.Equal($"Disk {_smallDisk} is not in range.", exception.Message);
-    }
-
-    [Fact]
-    public void DriveIcons_DiskCharIsTooSmall_DeletingPathThrows()
-    {
-        var exception = Assert.Throws<RegistryException>(
-            () => _driveIcons[_smallDisk] = null
-        );
-        ;
-        Assert.IsType<RegistryException>(exception);
-        Assert.Equal($"Disk {_smallDisk} is not in range.", exception.Message);
-    }
-
-    [Fact]
-    public void DriveIcons_DiskCharIsTooLarge_ReadingPathThrows()
-    {
-        var exception = Assert.Throws<RegistryException>(
-            () => _driveIcons[_largeDisk]
-        );
-        Assert.IsType<RegistryException>(exception);
-        Assert.Equal($"Disk {_largeDisk} is not in range.", exception.Message);
-    }
-
-    [Fact]
-    public void DriveIcons_DiskCharIsTooLarge_WritingPathThrows()
-    {
-        var exception = Assert.Throws<RegistryException>(
-            () => _driveIcons[_largeDisk] = "foo"
-        );
-        ;
-        Assert.IsType<RegistryException>(exception);
-        Assert.Equal($"Disk {_largeDisk} is not in range.", exception.Message);
-    }
-
-    [Fact]
-    public void DriveIcons_DiskCharIsTooLarge_DeletingPathThrows()
-    {
-        var exception = Assert.Throws<RegistryException>(
-            () => _driveIcons[_largeDisk] = null
-        );
-        ;
-        Assert.IsType<RegistryException>(exception);
-        Assert.Equal($"Disk {_largeDisk} is not in range.", exception.Message);
     }
 }
