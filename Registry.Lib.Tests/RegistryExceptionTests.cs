874729b67872dcf4fba41738bf0d2c52f9638a37
Alexey Vyskubov <alexey@ocaml.nl>
2022-11-14 17:29:11 +0200

    Tests for censtuctors of custom exception.

---
+++ Registry.Lib.Tests/RegistryExceptionTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Fi.Pentode.Registry.Lib.Test;
+
+public class RegistryExceptionTests
+{
+    [Fact]
+    public void RegistryException_BasicConstructorIsCalleed_ItWorks()
+    {
+        var exception = new RegistryException();
+        Assert.IsType<RegistryException>(exception);
+    }
+
+    [Fact]
+    public void RegistryException_ConstuctorWithMessage_SetsMessage()
+    {
+        var exception = new RegistryException("foobar");
+        Assert.IsType<RegistryException>(exception);
+        Assert.Equal("foobar", exception.Message);
+    }
+
+    [Fact]
+    public void RegistryException_ConstructorWithMessageAndCause_SetsBoth()
+    {
+        var inner = new Exception();
+        var exception = new RegistryException("foobar", inner);
+        Assert.IsType<RegistryException>(exception);
+        Assert.Equal("foobar", exception.Message);
+        Assert.True(ReferenceEquals(exception.InnerException, inner));
+    }
+}
